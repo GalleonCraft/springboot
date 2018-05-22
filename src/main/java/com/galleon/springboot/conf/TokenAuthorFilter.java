d609919d58c57bb5e3b060652655b9aa87237865
GalleonCraft <406525372@qq.com>
2018-05-22 16:12:55 +0800

    加入JWT过滤

---
+++ src/main/java/com/galleon/springboot/conf/TokenAuthorFilter.java
@@ -0,0 +1,60 @@
+package com.galleon.springboot.conf;
+
+import com.galleon.springboot.util.JsonWebToken;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by @author 付永强
+ * on 2018/5/10
+ **/
+@Slf4j
+@Configuration
+@WebFilter(filterName="tokenAuthorFilter")
+public class TokenAuthorFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request= (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        request.setAttribute("requesterIp",ip);
+        try {
+            JsonWebToken.verifyToken(request.getHeader("token"),ip);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
--- src/main/java/com/galleon/springboot/util/JsonWebToken.java
+++ src/main/java/com/galleon/springboot/util/JsonWebToken.java
@@ -6,6 +6,8 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.galleon.springboot.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -17,13 +19,14 @@ import java.util.Map;
  * on 2018/3/12
  **/
 public class JsonWebToken {
+    private static final Logger logger = LoggerFactory.getLogger(JsonWebToken.class);
     private static final String SECRET = "Winner!Winner!Chicken!Dinner!";
     /**
      * Create Token
      *
      * @throws Exception
      */
-    public static String createToken(User user) throws Exception{
+    public static String createToken(User user,String requesterIp) throws Exception{
 
         Date initDate = new Date();
         Calendar currentTime = Calendar.getInstance();
@@ -37,20 +40,23 @@ public class JsonWebToken {
                 .withHeader(map)
                 .withClaim("name",user.getUsername())
                 .withClaim("role",user.getRole().getName())
-                .withExpiresAt(expiresTime)//添加过气时间
-                .withIssuedAt(initDate)//添加签发时间
-                .sign(Algorithm.HMAC256(SECRET));
+                //添加过气时间
+                .withExpiresAt(expiresTime)
+                //添加签发时间
+                .withIssuedAt(initDate)
+                .sign(Algorithm.HMAC256(SECRET+requesterIp));
         return token;
     }
 
-    public static Map<String,Claim> verifyToken(String token) throws Exception{
-        JWTVerifier verifier = JWT.require(Algorithm.HMAC256(SECRET)).build();
-        DecodedJWT jwt;
+    public static Map<String,Claim> verifyToken(String token,String requesterIp) throws Exception{
+        JWTVerifier verifier = JWT.require(Algorithm.HMAC256(SECRET+requesterIp)).build();
+
         try{
-            jwt=verifier.verify(token);
+            DecodedJWT jwt=verifier.verify(token);
+            return jwt.getClaims();
         }catch (Exception e){
-            throw new RuntimeException("凭证已过气！");
+            logger.error(e.getMessage());
         }
-        return jwt.getClaims();
+        return null;
     }
 }
